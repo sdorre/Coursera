6a3f8f99a3354cc945a57ccc3811516a12552614
sdorre <stephane.dorre@gmail.com>
2014-03-05 10:54:09 +0100

    first draft, no tested

--- iRemember-Skeleton/src/edu/vuum/mocca/ui/story/CreateStoryActivity.java
+++ iRemember-Skeleton/src/edu/vuum/mocca/ui/story/CreateStoryActivity.java
@@ -61,6 +61,7 @@ import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.DialogFragment;
 import android.util.Log;
 import android.view.View;
@@ -231,19 +232,19 @@ public class CreateStoryActivity extends StoryActivityBase {
 	private void launchSoundIntent() {
 		
 		// TODO - Create a new intent to launch the SoundRecordActivity activity
-
+		Intent soundActivityIntent = new Intent(CreateStoryActivity.this, SoundRecordActivity.class);
 		
 		// TODO - Use getOutputMediaFile() to create a new 
 		// filename for this specific sound file
-		
+		File soundFile = getOutputMediaFile(MEDIA_TYPE_AUDIO);
 		
 		// TODO - Add the filename to the Intent as an extra. Use the Intent-extra name
 		// from the SoundRecordActivity class, EXTRA_OUTPUT
-
+		soundActivityIntent.putExtra(SoundRecordActivity.EXTRA_OUTPUT, soundFile);
 		
 		// TODO - Start a new activity for result, using the new intent and the request
 		// code MIC_SOUND_REQUEST
-		
+		startActivityForResult(soundActivityIntent, MIC_SOUND_REQUEST);
 	}
 	
 	// This function creates a new Intent to launch the built-in Camera activity
@@ -253,19 +254,19 @@ public class CreateStoryActivity extends StoryActivityBase {
 		// TODO - Create a new intent to launch the MediaStore, Image capture function
 		// Hint: use standard Intent from MediaStore class
 		// See: http://developer.android.com/reference/android/provider/MediaStore.html
-
+		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		
 		// TODO - Set the imagePath for this image file using the pre-made function
 		// getOutputMediaFile to create a new filename for this specific image;
-
+		File photoFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
 		
 		// TODO - Add the filename to the Intent as an extra. Use the Intent-extra name
 		// from the MediaStore class, EXTRA_OUTPUT
-		
+		cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile);
 		
 		// TODO - Start a new activity for result, using the new intent and the request
 		// code CAMERA_PIC_REQUEST
-		
+		startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
 
 	}
 
@@ -275,27 +276,24 @@ public class CreateStoryActivity extends StoryActivityBase {
 		// TODO - Create a new intent to launch the MediaStore, Image capture function
 		// Hint: use standard Intent from MediaStore class
 		// See: http://developer.android.com/reference/android/provider/MediaStore.html
-
+		Intent cameraVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
 		
 		// TODO - Set the fileUri for this video file using the pre-made function
 		// getOutputMediaFile to create a new filename for this specific video;
-		
-
+		File videoFile = getOutputMediaFile(MEDIA_TYPE_VIDEO);
 		
 		// TODO - Add the filename to the Intent as an extra. Use the Intent-extra name
 		// from the MediaStore class, EXTRA_OUTPUT
-
+		cameraVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, videoFile);
 		
 		// TODO - Specify as an extra that the video quality should be HIGH. Use the
 		// Intent-extra name, EXTRA_VIDEO_QUALITY, from the MediaStore class
 		// set the video image quality to high 
-		
+		cameraVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
 
-		
 		// TODO - Start a new activity for result, using the new intent and the request
 		// code CAMERA_VIDEO_REQUEST
-		
-
+		startActivityForResult(cameraVideoIntent, CAMERA_VIDEO_REQUEST);
 	
 	}
 
--- iRemember-Skeleton/src/edu/vuum/mocca/ui/story/StoryViewFragment.java
+++ iRemember-Skeleton/src/edu/vuum/mocca/ui/story/StoryViewFragment.java
@@ -226,7 +226,7 @@ public class StoryViewFragment extends Fragment {
 			// as a ringtone and play it back as such. Use the RingtonManager function getRingtone on
 			// the audioLinkPath to create the ringtone
 			
-			final Ringtone ringtone = null;
+			final Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), Uri.parse(audioLinkPath));
 			
 			
 			audioButton.setOnClickListener(new OnClickListener() {
@@ -234,7 +234,7 @@ public class StoryViewFragment extends Fragment {
 				public void onClick(View v) {
 					
 					// TODO - Play the ringtone
-					
+					ringtone.play();
 
 					
 				}
@@ -248,23 +248,23 @@ public class StoryViewFragment extends Fragment {
 			// and the video view already set up in the layout file.
 			
 			// TODO - Create a new MediaController for this activity 
-		
-
+			MediaController mediaController = new MediaController(getActivity());
+			
 			// TODO - The MediaController needs an anchorview. Anchor the Media Controller
 			// to the VideoView, videoLinkView, with the function setAnchorView()
-			
+			mediaController.setAnchorView(videoLinkView);
 			
 			
 			// TODO - Now the VideoView, videoLinkView, needs to have a Media Controller set to it
 			// use the setMediaController function from the VideoView to set it to the new Media Controller
-			
+			videoLinkView.setMediaController(mediaController);
 			
 			// TODO - Now we need to set the URI for the VideoView, use the setVideoURI function on the
 			//  videoLinkPath string from before.
-			
+			videoLinkView.setVideoURI(Uri.parse(videoLinkPath));
 			
 			// TODO - Start the video, using the start function on the VideoView
-			
+			videoLinkView.start();
 			
 			// Display the image data
 			
@@ -274,7 +274,7 @@ public class StoryViewFragment extends Fragment {
 			
 			// TODO - Set the URI of the ImageView to the image path stored in the string
 			// imageMetaDataPath, using the setImageURI function from the ImageView
-			
+			imageMetaDataView.setImageURI(Uri.parse(imageMetaDataPath));
 			
 			
 			Long time = Long.valueOf(storyData.storyTime);
